5706ffb90688800aea9dc95093677218eceb8a77
Yevhen Haievyi <yevhen.haievyi@gmail.com>
2023-02-23 09:53:48 +0200

    test2

--- next.config.js
+++ next.config.js
@@ -1,8 +1,27 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
+  images: { domains: ['eos.mypinata.cloud'] },
+  reactStrictMode: true,
   experimental: {
     appDir: true,
   },
-}
-
-module.exports = nextConfig
+  compiler: {
+    styledComponents: true,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+  modularizeImports: {
+    "@mui/icons-material": {
+      transform: "@mui/icons-material/{{member}}",
+    },
+    "@mui/material": {
+      transform: "@mui/material/{{member}}",
+    },
+  },
+  images: {
+    domains: ['eos.mypinata.cloud'],
+  },
+};
